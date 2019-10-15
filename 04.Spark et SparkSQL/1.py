fb69f5b0ecada555881a4a4441b0d59cef09aa82
Olivier Brunet <45876462+obrunet@users.noreply.github.com>
2019-10-15 09:16:53 +0200

    Add files via upload

---
+++ 04.Spark et SparkSQL/1.py
@@ -0,0 +1,18 @@
+﻿class MaClasse(object):
+
+    # constructeur
+    def __init__(self, param1, param2):
+        self.membre1 = param1
+        self.membre2 = param2
+
+    # affichage (equivalent de toString())
+    def __str__(self):
+        return "MaClasse['%s', %s]" % (self.membre1, self.membre2)
+
+    # accesseur
+    def getMembre1(self):
+        return self.membre1
+
+instance = MaClasse("pi", 3.1415);
+print instance
+print instance.getMembre1()
---
+++ 04.Spark et SparkSQL/2.py
@@ -0,0 +1,2 @@
+﻿brut = sc.textFile("hdfs:/share/noaa/isd-history.txt")
+brut = brut.filter(lambda ligne: len(ligne)>0 and ligne[0] in '0123456789')
